e82757ad2b20c8437d889c9d787d2a809798ef19
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-09-02 21:45:45 +0600

    Add files via upload

---
+++ Solved - Beginner Problemset/1016.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.util.Scanner;
+// Problem 1016 - Distance...
+// SUBMISSION # 41197800...
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+         
+         Scanner scanner = new Scanner(System.in);
+
+        // Read the input distance
+        int distance = scanner.nextInt();
+
+        // Calculate the time required in minutes
+        int time = distance * 2;
+
+        // Print the result followed by "minutos"
+        System.out.println(time + " minutos");
+    }
+ 
+}
