a00049c83c78633e6761d6e4686bfc62ee4e6d7f
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-09-02 21:44:31 +0600

    Add files via upload

---
+++ Solved - Beginner Problemset/1011.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.util.Scanner;
+// Proble 1011 - Sphere...
+// SUBMISSION # 41183091...
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+         Scanner input = new Scanner(System.in);
+         int r = input.nextInt();
+         
+         double pi = 3.14159;
+         
+         double sphere = 4.0/3.0 * pi * r * r * r;
+         
+         System.out.printf("VOLUME = %.3f%n", sphere);
+ 
+    }
+ 
+}
